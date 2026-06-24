09a40fa7e48252aed1e5e7e76e87ea079345a8eb
Aanand-Anupam <AnupamAanandthakur@gmail.com>
2026-06-24 22:25:43 +0530

    improved multer logic

--- server/src/middleware/multer.middleware.ts
+++ server/src/middleware/multer.middleware.ts
@@ -1,13 +1,18 @@
 import multer from "multer";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = dirname(__fileName);
+const uploadDir = path.join(__dirName, "../../uploads");
 
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirName, "../../uploads"));
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniquePrefix = Date.now() + "-" + Math.round(Math.random() * 1e9);
